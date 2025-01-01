eb348529f9703be9b7a19120386ea8cf959afda6
Eylemky <eylemky1998@hotmail.com>
2025-01-01 17:51:28 +0300

    Hatalar düzeltildi

--- ETicaretMVC/Models/ViewModels/LoginVM.cs
+++ ETicaretMVC/Models/ViewModels/LoginVM.cs
@@ -5,20 +5,8 @@ namespace ETicaretMVC.Models.ViewModels
     public class LoginVM
     {
         [DataType(DataType.EmailAddress)]
-        [Required(ErrorMessage = "İsim girilmek zorundadir")]
-        public string Name { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        [Required(ErrorMessage = "Soyisim girilmek zorundadir")]
-        public string Surname { get; set; }
-
-        [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "Email Adresi girilmek zorundadir")]
         public string Email { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        [Required(ErrorMessage = "Telefon Numarası girilmek zorundadir")]
-        public string PhoneNumber { get; set; }
 
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Şifre  girilmesi zorunludur")]
